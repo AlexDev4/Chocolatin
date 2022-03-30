bc0893d24567bd0d5bf0b3f544d37c3c850a8e93
AlexDev4 <92247996+AlexDev4@users.noreply.github.com>
2022-03-30 13:41:02 +0200

    Index terminé à 80%

---
+++ footer.php
@@ -0,0 +1,31 @@
+<div class="footer">
+    <div class="slogan">
+    <p>Des chocolats artisanaux de qualité exceptionnelle à Bordeaux ! Tout le chocolat que nous vendons est 100% pur beurre de cacao et à la vanille naturelle.</p>
+    <img src="assets/label_bio-partenaire.png"
+         alt="label bio-partenaire"
+         id="bio">
+    </div>
+    <div class = "menufooter">
+        <h3>Nos catégories</h3>
+        <p>Produits de saison</p>
+        <p>Chocolats/Assortiments</p>
+        <p>Confiseries</p>
+        <p>Dragées et cadeaux invités</p>
+        <p>Idéees cadeaux</p>
+    </div>
+    <div class="infolettre">
+    <h3>Infolettre</h3>
+        <p>Abonnez-vous à notre infolettre pour être toujours informé(e) de nos dernières nouveautés ! Notre lettre d'informations est envoyée environ une fois par mois.</p>
+        <div>
+        <form action="" method="get" class="formInfolettre"></form>
+        <input type="email" name="email" value="Entrez votre email" required>
+        <button id="btnInfolettre"><img src="assets/icone%20infolettre.png" id="imgInfolettre"></button>
+        </div>
+    </div>
+    </div>
+<div class="CGU">
+    <p id="CG">Consultez nos conditions générales d'utilisation</p>
+    <p>©2015-2021 Chocolate'in - Créé par José GIL - Tous droits réservés</p>
+    <p>Depuis 2015, retrouvez nous au 1 place du Parlement à Bordeaux</p>
+    <p>N'hésitez pas à nous appeler au 05 12 34 56 78</p>
+</div>
